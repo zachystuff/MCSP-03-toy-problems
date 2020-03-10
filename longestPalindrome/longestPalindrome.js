3381076e7249a9140f7fcf04a083b1caf01b4052
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-03-10 10:03:50 -0500

    Longest Palindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,4 +6,33 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+function stringConcat(first, mid, last) {
+    return first + mid + last
+}
+
+var longestPalindrome = function(string) {
+    const palindromeStore = [];
+
+    if(string.length === 1) {
+        return string;
+    }
+
+    for(let i = 1; i < string.length; i++) {
+        if (string[i - 1] == string[i + 1]) {
+            let palindrome = string[i-1] + string[i] + string[i+1];
+            console.log(palindrome);
+            palindromeStore.push(palindrome);
+            let newIndex = string.indexOf(palindrome);
+            console.log(newIndex);
+            do {
+                palindrome = string[newIndex - 1] + palindrome + string[palindrome.length + 1];
+                palindromeStore.push(palindrome);
+            } while ((string[newIndex - 1] == string[palindrome.length + 1]))
+        }
+    }
+
+    return palindromeStore;
+    
+};
+
+console.log(longestPalindrome("My dad is a racecar athlete"));
