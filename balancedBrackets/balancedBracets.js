784200fbf152d89be8d0eab5a9c7d7c5fefcf1d5
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-03-20 09:11:19 -0500

    balancedBrackets

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -7,6 +7,36 @@
 // str: "(x + y) - (4)"           |   true
 // str: "(((10 ) ()) ((?)(:)))"   |   true
 // str: "[{()}]"                  |   true
-// str: "(50)("                   |   false
+// str: "(50)("                   |   false 
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    const store = [];
+    for(let i of str) { 
+        console.log(i)
+        if (i == '(' || i == '[' || i == '{') {
+            store.push(i)
+        }
+        if (i == ')') {
+            if (store[store.length - 1] == '(') {
+                store.pop();
+            }
+        }
+        if (i == ']') {
+            if(store[store.length - 1] == '[') {
+                store.pop();
+            }
+        }
+        if(i == '}') {
+            
+        }  
+            
+    }
+    console.log(store);
+    return !(store.length > 0);
+};
+
+console.log(isBalanced("(x + y) - (4)"))
+console.log(isBalanced("[{]}"))
+console.log(isBalanced("(x + y) - (4)"))
+console.log(isBalanced("(x + y) - (4)"))
+console.log(isBalanced("(x + y) - (4)"))
